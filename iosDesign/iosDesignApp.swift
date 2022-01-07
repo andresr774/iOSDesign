931107fd182f75bdde6ca17c3ff9c1bfa1154634
Andres <andresr774@gmail.com>
2022-01-07 10:00:10 -0500

    Initial Commit

---
+++ iosDesign/iosDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosDesignApp.swift
+//  iosDesign
+//
+//  Created by Andres camilo Raigoza misas on 7/01/22.
+//
+
+import SwiftUI
+
+@main
+struct iosDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
