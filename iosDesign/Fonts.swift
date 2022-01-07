4ae369b2dbfb2b6be6f809996e26d7f5e7899cb6
Andres <andresr774@gmail.com>
2022-01-07 10:12:08 -0500

    - initial commit

---
+++ iosDesign/Fonts.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  iosDesign
+//
+//  Created by Andres camilo Raigoza misas on 7/01/22.
+//
+
+import SwiftUI
+
+struct Fonts: View {
+    
+    init() {
+        for famililyName in UIFont.familyNames {
+            print(famililyName)
+            
+            for fontName in UIFont.fontNames(forFamilyName: famililyName) {
+                print("-- \(fontName)")
+            }
+        }
+    }
+    
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct Fonts_Previews: PreviewProvider {
+    static var previews: some View {
+        Fonts()
+    }
+}
--- iosDesign/iosDesignApp.swift
+++ iosDesign/iosDesignApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct iosDesignApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Fonts()
         }
     }
 }
